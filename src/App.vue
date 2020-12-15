edca0315bca2769a18aa95044396cfd8d0ceb645
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2020-12-15 11:49:08 +0300

    newrange

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	  <div class="container">
+	  	<div>
+			<label><input type="range" min="1" max="5" step="1" v-model="valueColumns">Кол-во колонок: {{ valueColumns }}</label>
+			<label><input type="range" min="1" max="3" step="1" v-model="valueRows">Кол-во рядов: {{ valueRows }}</label>
+			<label><input type="text" v-model="border.width">Толщина рамки</label>
+			<label><input type="color" v-model="border.color">Цвет рамки</label>
+			<select v-model="border.type">
+				<option value="solid" selected>Сплошная</option>
+				<option value="dotted">Пунктирная</option>
+				<option value="dashed">Штриховая</option>
+				<option value="double">Двойная</option>
+				<option value="groove">Вдавленная</option>
+				<option value="ridge">Рельефная</option>
+				<option value="inset">Внутрь</option>
+				<option value="outset">Наружу</option>
+			</select>
+			<label><input type="color" v-model="text.color">Цвет текста</label>
+			<label><input type="text" v-model="text.size">Размер шрифта текста</label>
+			<label><input type="text" v-model="text.padding">Отступы от текста (сверху, справа, снизу, слева)</label>
+			<label><input type="text" v-model="picture.padding">Отступы от картинки (сверху, справа, снизу, слева)</label>
+		</div>
+
+		<div class="row">
+			<Column
+				v-for="column of getColumns" :key="column.id"
+				v-bind:column="column"
+				v-bind:valueColumns="+valueColumns"
+				v-bind:border="border"
+				v-bind:text="text"
+				v-bind:picture="picture"
+			/>
+		</div>
+	  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Column from '@/components/Column'
 
 export default {
+  data() {
+	return {
+		columns: [
+			{id: 1, title: 'Колонка 1'},
+			{id: 2, title: 'Колонка 2'},
+			{id: 3, title: 'Колонка 3'},
+			{id: 4, title: 'Колонка 4'},
+			{id: 5, title: 'Колонка 5'}
+		],
+		border: {
+			width: '0px',
+			type: 'solid',
+			color: '#000'
+		},
+		text: {
+			color: '#000',
+			size: "20px",
+			padding: "0px 0px 0px 0px"
+		},
+		picture: {
+			padding: "0px 0px 0px 0px"
+		},
+		valueColumns: 3,
+		valueRows: 1
+	}
+  },
   name: 'App',
   components: {
-    HelloWorld
+	Column
+  },
+  computed: {
+	getColumns() {
+		let result = [];
+		
+		for (let i = 0 ; i < this.valueColumns * this.valueRows; i++) {
+			result.push({id: i+1, title: `Колонка ${i}`})
+		}
+
+		return result;
+	}
   }
 }
 </script>
@@ -24,5 +92,43 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+	padding: 0px;
+	margin: 0px;
+	border: 0px;
+}
+*,
+*:before,
+*:after {
+	-moz-box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	box-sizing: border-box;
+}
+.container {
+	max-width: 1230px;
+	padding: 0 15px;
+	margin: 0 auto;
+}
+.row {
+	display: flex;
+	margin: 0 -10px;
+	flex-wrap: wrap;
+}
+input {
+	border: 1px solid #000;
+	margin: 0px 5px 0px 0px;
+	padding: 2px;
+}
+label {
+	display: flex;
+	text-align: left;
+	margin: 0px 0px 10px 0px;
+}
+
+select {
+	border: 1px solid #000;
+	display: block;
+	margin: 0px 0px 10px 0px;
 }
 </style>
---
+++ src/components/Column.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="column" v-bind:style="{flex: `${flexProp}`}">
+	<div class="border" v-bind:style="{border: `${border.width} ${border.type} ${border.color}`}">
+		<div class="image">
+			<img src="@/assets/01.jpg" alt="" v-bind:style="{padding: `${picture.padding}`}">
+		</div>
+		<div class="text" v-bind:style="{color: `${text.color}`, 'font-size': `${text.size}`, padding: `${text.padding}`}">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error praesentium asperiores totam. Laborum quis unde perferendis qui labore!
+		</div>
+	</div>
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+		valueColumns: Number,
+		column: Object,
+		border: Object,
+		text: Object,
+		picture: Object
+	},
+	computed: {
+		flexProp() {
+			if (this.valueColumns == 1) {
+				return '1 1 100%'
+			}
+			if (this.valueColumns == 2) {
+				return '0 1 50%'
+			}
+			if (this.valueColumns == 3) {
+				return '0 1 33.333%'
+			}
+			if (this.valueColumns == 4) {
+				return '0 1 25%'
+			}
+			if (this.valueColumns == 5) {
+				return  '0 1 20%'
+			}
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.column {
+	padding: 0 10px;
+	margin: 0px 0px 20px 0px;
+}
+.image {
+	position: relative;
+	height: 300px;
+	min-width: 200px;
+	
+	img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		object-fit: cover;
+	}
+}
+.text {
+	line-height: 1.2;
+}
+</style>
